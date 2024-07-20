2e40171f98f33e9d9f7ea6d8120ce4017b1166de
Miku404 <82779172+krisstibex@users.noreply.github.com>
2024-07-20 20:04:20 +0800

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -5,6 +5,8 @@ $folderPath = Join-Path -Path $desktopPath -ChildPath "sing-box"
 New-Item -ItemType Directory -Path $folderPath -Force
 Set-Location -Path $folderPath
 
+Clear-Host
+
 Write-Host ""
 Write-Host "================================"
 Write-Host " Sing-Box Manager Downloader"
@@ -13,10 +15,10 @@ Write-Host ""
 
 $userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 
-Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/DustinWin/clash_singbox-tools@main/sing-box-release/sing-box-windows-amd64.exe" -OutFile "sing-box.exe" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent }
-Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/winsw/winsw@releases/latest/download/WinSW-x64.exe" -OutFile "sing-box-service.exe" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent }
-Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/krisstibex/sing-box-manager@main/sing-box-manager.ba" -OutFile "sing-box-manager.bat" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent }
-Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/krisstibex/sing-box-manager@main/sing-box-service.xml" -OutFile "sing-box-service.xml" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent }
+Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/DustinWin/clash_singbox-tools@main/sing-box-release/sing-box-windows-amd64.exe" -OutFile "sing-box.exe" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent } > $null 2>&1
+Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/winsw/winsw@releases/latest/download/WinSW-x64.exe" -OutFile "sing-box-service.exe" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent } > $null 2>&1
+Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/krisstibex/sing-box-manager@main/sing-box-manager.ba" -OutFile "sing-box-manager.bat" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent } > $null 2>&1
+Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/krisstibex/sing-box-manager@main/sing-box-service.xml" -OutFile "sing-box-service.xml" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent } > $null 2>&1
 
 Write-Host "Done."
 Start-Sleep -Seconds 5
