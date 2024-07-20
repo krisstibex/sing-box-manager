cfbdf613329eece64600bd35af272da8f042c030
Miku404 <82779172+krisstibex@users.noreply.github.com>
2024-07-20 20:09:05 +0800

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -15,10 +15,10 @@ Write-Host ""
 
 $userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 
-Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/DustinWin/clash_singbox-tools@main/sing-box-release/sing-box-windows-amd64.exe" -OutFile "sing-box.exe" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent } > $null 2>&1
-Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/winsw/winsw@releases/latest/download/WinSW-x64.exe" -OutFile "sing-box-service.exe" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent } > $null 2>&1
-Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/krisstibex/sing-box-manager@main/sing-box-manager.ba" -OutFile "sing-box-manager.bat" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent } > $null 2>&1
-Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/krisstibex/sing-box-manager@main/sing-box-service.xml" -OutFile "sing-box-service.xml" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent } > $null 2>&1
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DustinWin/clash_singbox-tools/main/sing-box-release/sing-box-windows-amd64.exe" -OutFile "sing-box.exe" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent } > $null 2>&1
+Invoke-WebRequest -Uri "https://github.com/winsw/winsw/releases/latest/download/WinSW-x64.exe" -OutFile "sing-box-service.exe" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent } > $null 2>&1
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/krisstibex/sing-box-manager/main/sing-box-manager.bat" -OutFile "sing-box-manager.bat" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent } > $null 2>&1
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/krisstibex/sing-box-manager/main/sing-box-service.xml" -OutFile "sing-box-service.xml" -ErrorAction Stop -Headers @{ "User-Agent" = $userAgent } > $null 2>&1
 
 Write-Host "Done."
 Start-Sleep -Seconds 5
