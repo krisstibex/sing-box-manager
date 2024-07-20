7bcaf18be2158203890f31bc7f1cc2655c9867cb
Miku404 <82779172+krisstibex@users.noreply.github.com>
2024-07-20 18:09:12 +0800

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -11,9 +11,9 @@ if not exist "%FolderPath%" (
 
 cd /d "%FolderPath%"
 
-curl -s "https://mirror.ghproxy.com/https://raw.githubusercontent.com/DustinWin/clash_singbox-tools/main/sing-box-release/sing-box-windows-amd64.exe" -o sing-box.exe
-curl -s "https://mirror.ghproxy.com/https://github.com/winsw/winsw/releases/latest/download/WinSW-x64.exe" -o sing-box-service.exe
-curl -s "https://mirror.ghproxy.com/https://gist.githubusercontent.com/krisstibex/aec44f359d82c8694c4619c40c902e96/raw/sing-box-manager.bat" -o sing-box-manager.bat
-curl -s "https://mirror.ghproxy.com/https://gist.githubusercontent.com/krisstibex/aec44f359d82c8694c4619c40c902e96/raw/sing-box-service.xml" -o sing-box-service.xml
+curl -s "https://cdn.jsdelivr.net/gh/DustinWin/clash_singbox-tools@main/sing-box-release/sing-box-windows-amd64.exe" -o sing-box.exe
+curl -s "https://cdn.jsdelivr.net/gh/winsw/winsw@releases/latest/download/WinSW-x64.exe" -o sing-box-service.exe
+curl -s "https://cdn.jsdelivr.net/gh/krisstibex/sing-box-manager@main/sing-box-manager.bat" -o sing-box-manager.bat
+curl -s "https://cdn.jsdelivr.net/gh/krisstibex/sing-box-manager@main/sing-box-service.xml" -o sing-box-service.xml
 
 endlocal
