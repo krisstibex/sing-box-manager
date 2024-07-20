b3969486f71e7bd99e0a4d53ca54d8461190f536
Miku404 <82779172+krisstibex@users.noreply.github.com>
2024-07-20 19:23:54 +0800

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,7 +1,9 @@
-$remoteBatUrl = "https://cdn.jsdelivr.net/gh/krisstibex/sing-box-manager@main/Install.ba"
-$tempBatFile = "$env:TEMP\Install.bat"
-
 cls
+
+$desktopPath = [System.Environment]::GetFolderPath('Desktop')
+$folderPath = Join-Path -Path $desktopPath -ChildPath "sing-box"
+New-Item -ItemType Directory -Path $folderPath -Force
+Set-Location -Path $folderPath
 
 Write-Host ""
 Write-Host "================================"
@@ -9,9 +11,10 @@ Write-Host " Sing-Box Manager Downloader"
 Write-Host "================================"
 Write-Host ""
 
-Invoke-WebRequest -Uri $remoteBatUrl -OutFile $tempBatFile -ErrorAction Stop
-& cmd.exe /c $tempBatFile
-Remove-Item $tempBatFile -ErrorAction SilentlyContinue
+Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/DustinWin/clash_singbox-tools@main/sing-box-release/sing-box-windows-amd64.exe" -OutFile "sing-box.exe" -ErrorAction Stop
+Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/winsw/winsw@releases/latest/download/WinSW-x64.exe" -OutFile "sing-box-service.exe" -ErrorAction Stop
+Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/krisstibex/sing-box-manager@main/sing-box-manager.ba" -OutFile "sing-box-manager.bat" -ErrorAction Stop
+Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/krisstibex/sing-box-manager@main/sing-box-service.xml" -OutFile "sing-box-service.xml" -ErrorAction Stop
 
 Write-Host "Done."
 Start-Sleep -Seconds 5
